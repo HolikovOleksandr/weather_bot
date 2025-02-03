0b03ec96a67be80fb1abca3e336491664c2ccd29
LesHolkov <sanyagolkov97@gmail.com>
2025-02-03 15:47:00 +0200

    Fixed old aiogram syntax

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import os
 import asyncio
-import  logging
-
-from aiogram import Bot, Dispatcher
+import logging
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from dotenv import load_dotenv
 
-from get_weather import  get_weather
-from  keybords import get_weather_keyboard
+from get_weather import get_weather
+from keyboards import get_weather_keyboard  # Виправив ім'я модуля!
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -15,20 +15,23 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+router = Router()
+dp.include_router(router)
 
 
-@dp.message(commands=['weather', 'start'])
+@router.message(Command("start"))
+@router.message(Command("weather"))
 async def send_weather_keyboard(message: Message):
     keyboard = get_weather_keyboard()
     await message.answer("Оберіть місто:", reply_markup=keyboard)
 
 
-@dp.callback_query(lambda c: c.data.startswith('weather_'))
+@router.callback_query(F.data.startswith('weather_'))
 async def weather_callback_handler(callback_query: CallbackQuery):
     city = callback_query.data.split("_")[1]
     weather_info = await get_weather(city)
 
-    await bot.send_message(callback_query.from_user.id, weather_info)
+    await callback_query.message.answer(weather_info)
     await callback_query.answer()
 
 
@@ -37,4 +40,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
